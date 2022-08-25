0a09da451a77f9e89531f2331241348224e1b7e5
Firoz Ansari <firozansar@gmail.com>
2022-08-25 07:04:24 +0100

    Initial setup completed

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "1.6.20"
+    application
+}
+
+group = "info.firozansari"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+
+    testImplementation(kotlin("test-junit5"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+}
+
+tasks.test { useJUnitPlatform() }
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>() { kotlinOptions.jvmTarget = "11" }
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+
+
+fun main(args: Array<String>) {
+    println("What is your name?")
+    val name = readln()
+    println("Hello $name")
+}
