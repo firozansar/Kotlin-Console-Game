c01cffeaee3438421db00952ec866eff60e71615
Firoz Ansari <firozansar@gmail.com>
2022-08-25 07:57:39 +0100

    Refactoring

--- src/main/kotlin/info/firozansari/Game.kt
+++ src/main/kotlin/info/firozansari/Game.kt
@@ -1,35 +1,34 @@
 package info.firozansari
+
 import java.util.*
 import java.util.concurrent.TimeUnit
 
-data class Player(val name:String?, var health:Int)
+data class Player(val name: String?, var health: Int)
 
-class Game{
-    // define the game vars
-    var randCh: (Int, Int) -> Int = { min, max -> (min..max).random()}
-    private var enemyList = listOf<String>("spider", "cyto", "goblin", "enzo", "el0xren", "boss cyto", "boss el0xren", "boss enzo", "boss goblin", "boss spider", "ahmed", "boss ahmed")
+class Game {
+    var randChar: (Int, Int) -> Int = { min, max -> (min..max).random() }
+    private var enemyList = listOf("Boss Cyto", "Boss Enzo", "Boss Goblin", "Boss Spider", "Spider", "Cyto", "Goblin", "Enzo")
     private val enemy = enemyList.random()
-    private var hp = if("boss" in enemy) (150..200).random() else 100
+    private var hp = if ("Boss" in enemy) (150..200).random() else 100
     private var hpPacks = (1..3).random()
 
-    //the game function
-    fun playGame(player:Player) {
+    fun playGame(player: Player) {
         val enemy = Player(enemy, hp)
-        player.health = if(randCh(1,10) == 5) 125 else 100
+        player.health = if (randChar(1, 10) == 5) 125 else 100
         TimeUnit.SECONDS.sleep(1L)
         println("Welcome ${player.name} to this very epic dungeon game")
         TimeUnit.SECONDS.sleep(2L)
-        if(player.health == 125) println("\nyou're lucky! you just got 25+ hp!")
+        if (player.health == 125) println("\nyou're lucky! you just got 25+ hp!")
         TimeUnit.SECONDS.sleep(2L)
-        println("\nOH S**T A WILD ${enemy.name?.uppercase(Locale.getDefault())} APPEARS!")
+        println("\nOH no! a wild ${enemy.name?.uppercase(Locale.getDefault())} appears!")
         TimeUnit.SECONDS.sleep(2L)
-        println("\nGET READY FOR THE FIGHT ${player.name?.uppercase(Locale.getDefault())}")
+        println("\nGet ready for the fight ${player.name?.uppercase(Locale.getDefault())}!")
         TimeUnit.SECONDS.sleep(3L)
         var specialAttack = 6
 
         fun attack(): String? {
             println("\n\n${enemy.name}'s health: ${enemy.health}, ${player.name}'s health: ${player.health}\n(\"choose from the numbers below\")")
-            println("1.do a basic attack\n2.do a special attack \"has a (1/$specialAttack) chance of working\"\n3.heal up (current health packs = $hpPacks)\n: ")
+            println("1.Do a basic attack\n2.Do a special attack \"has a (1/$specialAttack) chance of working\"\n3.Heal up (current health packs = $hpPacks)\n: ")
             return readLine()
         }
 
@@ -38,39 +37,38 @@ class Game{
             val choice = attack()
             TimeUnit.SECONDS.sleep(1L)
 
-            when(choice){
+            when (choice) {
                 "1" -> {
                     enemy.health -= 15
-                    if(enemy.health <= 0) {
-                        println("\nyou've managed to kill ${enemy.name}")
+                    if (enemy.health <= 0) {
+                        println("\nWell done! You have managed to kill ${enemy.name}")
                         break
                     }
                     println("_".repeat(30))
-                    println("\ndoing a basic attack of 15! ${enemy.name}'s health is now ${enemy.health}")
-                    if (startFighting(enemy, player)) break
-
+                    println("\nDoing a basic attack of 15! ${enemy.name}'s health is now ${enemy.health}")
+                    if (startAttacking(enemy, player)) break
                 }
                 "2" -> {
                     println("_".repeat(30))
                     TimeUnit.SECONDS.sleep(1L)
-                    println("trying a special attack!")
+                    println("Trying a special attack!")
                     TimeUnit.SECONDS.sleep(1L)
-                    var worked:Boolean = false
+                    var worked: Boolean
                     if ((1..specialAttack).random() == 1) {
                         worked = true
-                        println("special attack worked!\ndecreasing the enemy's health by 40!")
+                        println("Special attack worked!\nDecreasing the enemy's health by 40!")
                         enemy.health -= 40
-                        if(enemy.health <= 0) {
-                            println("\nyou've managed to kill ${enemy.name}")
+                        if (enemy.health <= 0) {
+                            println("\nWell done! You have managed to kill ${enemy.name}")
                             break
                         }
                     } else {
                         TimeUnit.SECONDS.sleep(1L)
-                        println("Attack Failed!")
+                        println("Oh no! Attack Failed!")
                         worked = false
-                        if (startFighting(enemy, player)) break
+                        if (startAttacking(enemy, player)) break
                     }
-                    when(worked){
+                    when (worked) {
                         true -> specialAttack = 6
                         false -> specialAttack -= 1
                     }
@@ -78,13 +76,13 @@ class Game{
                 "3" -> {
                     println("_".repeat(30))
                     TimeUnit.SECONDS.sleep(1L)
-                    if(hpPacks > 0){
+                    if (hpPacks > 0) {
                         println("Healing up by 25!")
                         TimeUnit.SECONDS.sleep(1L)
                         player.health += 25
                         hpPacks -= 1
                     } else {
-                        println("you don't have enough health packs!")
+                        println("You don't have enough health packs!")
                     }
                     println("_".repeat(30))
 
@@ -97,25 +95,24 @@ class Game{
                     println("_".repeat(30))
                 }
             }
-        } while(enemy.health > 0 && player.health > 0)
+        } while (enemy.health > 0 && player.health > 0)
 
-        if(player.health <= 0){
-            println("you have lost!")
-        } else if(enemy.health <= 0){
-            println("you've won!")
+        if (player.health <= 0) {
+            println("You have lost!")
+        } else if (enemy.health <= 0) {
+            println("Well done! You have won!")
         } else {
-            println("you both have tied!")
+            println("You both have tied!")
         }
-
     }
 
-    private fun startFighting(enemy: Player, player: Player): Boolean {
-        println("${enemy.name} Attacks!")
-        val rand = randCh(1, 6)
+    private fun startAttacking(enemy: Player, player: Player): Boolean {
+        println("${enemy.name} attacks!")
+        val rand = randChar(1, 6)
         if (rand == 3) {
             println("${enemy.name} missed!")
         } else {
-            player.health -= randCh(10, 20)
+            player.health -= randChar(10, 20)
             if (player.health <= 0) {
                 println("\n${enemy.name} has managed to kill you!")
                 return true
