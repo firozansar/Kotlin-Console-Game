9f158a17653ce6dd2a9d4a0e4c1ba05cf948c08f
Firoz Ansari <firozansar@gmail.com>
2024-04-11 17:28:09 +0100

    Updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.6.20"
+    kotlin("jvm") version "1.9.23"
     application
 }
 
 group = "info.firozansari"
-version = "1.0-SNAPSHOT"
+version = "2.0"
 
 repositories {
     mavenCentral()
@@ -17,14 +17,17 @@ application {
 }
 
 dependencies {
-    implementation(kotlin("stdlib"))
-    implementation("com.github.ajalt.clikt:clikt:3.5.0")
-    implementation("com.github.ajalt.mordant:mordant:2.0.0-beta7")
+    implementation("com.github.ajalt.clikt:clikt:4.3.0")
+    implementation("com.github.ajalt.mordant:mordant:2.4.0")
 
     testImplementation(kotlin("test-junit5"))
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 }
 
-tasks.test { useJUnitPlatform() }
-tasks.withType<KotlinCompile>() { kotlinOptions.jvmTarget = "11" }
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}
